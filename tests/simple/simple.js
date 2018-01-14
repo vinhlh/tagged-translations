416de4702a4dfaf431539e4b29c01f41dc855047
Vinh Le <vinh.ninja@icloud.com>
2018-01-14 15:16:08 +0700

    Add test for wrong quasis, remove redundant data

--- tests/simple/simple.js
+++ tests/simple/simple.js
@@ -7,3 +7,11 @@ t`This text doesn't have any translation`
 t`${itemFound} items found!`
 
 t`Please make the transfer of ${grandTotal} to the following account within ${time} to complete your order`
+
+t`This ${text} has ${wrong} quasis`
+
+const awesomeNumber = 1 + '2'
+
+function anotherFunc() {
+  return true
+}
